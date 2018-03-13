08fb41845954b06a33013193ce2b8f9807c373ad
70 <412826828@qq.com>
2018-03-14 00:09:33 +0800

    1

---
+++ TcmHMS.Web/App/Main/app.js
@@ -0,0 +1,15 @@
+﻿var appModule = angular.module("app", [
+    "ui.router",
+    "ui.bootstrap",
+    'ui.utils',
+    "ui.jq",
+    'ui.grid',
+    'ui.grid.pagination',
+    "oc.lazyLoad",
+    "ngSanitize",
+    'angularFileUpload',
+    'daterangepicker',
+    'angularMoment',
+    'frapontillo.bootstrap-switch',
+    'abp'
+]);
